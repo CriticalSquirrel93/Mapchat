8600f59c73a7193b45c92d299d07b034ac1e49df
Aisling McIntosh <122208641+CriticalSquirrel93@users.noreply.github.com>
2023-11-15 18:37:42 -0800

    Delete all the things.

--- authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from authentication import views
-
-urlpatterns = [
-    path('login', views.login_firebase),
-    path('firebase_login_save', views.firebase_login_save),
-]
--- feed/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-### These are just notes I took while programing my files ###
-# my dict = {} ability to just pass in self-made dict as third agurment
-# each block content is unique to its html file
-def feed(request):
-    return render(request, 'homepage.html', {"name": "test"})
