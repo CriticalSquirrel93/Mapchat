ccacf806caf7f17d3ba68a1a5ba20a6158f9bd94
crunchywaffl <nico.stovall@gmail.com>
2023-11-20 21:33:20 -0800

    Adding geocode.js to contexts dir.

---
+++ src/contexts/geocode.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+export let getLocationData = () => {
+    const [locationData, setLocationData] = useState();
+    let zip;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                let lat = position.coords.latitude,
+                    long = position.coords.longitude,
+                    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+                        + lat + "," + long + "&result_type=postal_code&key=AIzaSyACt-GVyeV6bjLcEjSFLm_GMQy1HDJxiJQ";
+                fetch(url)
+                    .then(response => {
+                        if (response.ok) {
+                            console.log('Address data retrieved!');
+                        } else {
+                            console.error('Data NOT retrieved.');
+                        }
+                        response.json()
+                            .then(data => {
+                                console.log(data);
+                                setLocationData(data);
+                                zip = locationData.results[0].address_components[0].short_name
+                            });
+                        }
+                    )
+                    .catch(error => console.error(error))
+                // JSON result in `data` variable
+            }
+        );
+        /*
+        return {
+            lat: lat,
+            long: long,
+            zip: zipcode,
+            time: time
+        };
+        */
+    } else {
+        alert("Sorry, Geolocation is not supported by your browser.");
+    }
+
+}
